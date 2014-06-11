a42356da3a3936a6fdd77df72c87c84c95a1a765
Roger <roger-@users.noreply.github.com>
2014-06-11 18:32:39 -0500

    add test case

--- pandas/io/tests/test_pytables.py
+++ pandas/io/tests/test_pytables.py
@@ -1407,6 +1407,19 @@ class TestHDFStore(tm.TestCase):
             expected = expected[expected.reset_index(level=['major']).index.isin(['A','B'])]
             tm.assert_frame_equal(result.to_frame(),expected)
 
+    def test_append_resume_index(self):
+        df1 = DataFrame(np.random.randn(5,1),columns=['A'])
+        df2 = DataFrame(np.random.randn(5,1),columns=['A'])
+
+        expected = df1.append(df2, ignore_index=True)
+
+        with ensure_clean_store(self.path) as store:
+            store.append('test', df1, resume_index=False)
+            store.append('test', df2, resume_index=True)
+
+            result = store.get('test')
+            tm.assert_frame_equal(result, expected)
+            
     def test_create_table_index(self):
 
         with ensure_clean_store(self.path) as store:
